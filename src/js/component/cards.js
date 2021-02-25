68b2a1419f708ee84c066135e51b45f9c38ce445
jafinol <jafinol@gmail.com>
2021-02-25 00:51:23 +0000

    some change commit

--- src/js/component/cards.js
+++ src/js/component/cards.js
@@ -6,6 +6,24 @@ const Cards = () => {
 			<div className="row">
 				<div className="col-sm-3">
 					<div className="card">
+						<svg
+							className="bd-placeholder-img card-img-top"
+							width="100%"
+							height="180"
+							xmlns="http://www.w3.org/2000/svg"
+							role="img"
+							aria-label="Placeholder: Image cap"
+							preserveAspectRatio="xMidYMid slice"
+							focusable="false">
+							<title>Placeholder</title>
+							<rect
+								width="100%"
+								height="100%"
+								fill="#868e96"></rect>
+							<text x="35%" y="50%" fill="#dee2e6" dy=".3em">
+								500 X 325
+							</text>
+						</svg>
 						<div className="card-body">
 							<h5 className="card-title">Card Title</h5>
 							<p className="card-text">
@@ -13,13 +31,31 @@ const Cards = () => {
 								adipiscing elit.
 							</p>
 							<a href="#" className="btn btn-primary">
-								Go somewhere
+								Find Out More!
 							</a>
 						</div>
 					</div>
 				</div>
 				<div className="col-sm-3">
 					<div className="card">
+						<svg
+							className="bd-placeholder-img card-img-top"
+							width="100%"
+							height="180"
+							xmlns="http://www.w3.org/2000/svg"
+							role="img"
+							aria-label="Placeholder: Image cap"
+							preserveAspectRatio="xMidYMid slice"
+							focusable="false">
+							<title>Placeholder</title>
+							<rect
+								width="100%"
+								height="100%"
+								fill="#868e96"></rect>
+							<text x="35%" y="50%" fill="#dee2e6" dy=".3em">
+								500 X 325
+							</text>
+						</svg>
 						<div className="card-body">
 							<h5 className="card-title">Card Title</h5>
 							<p className="card-text">
@@ -27,13 +63,31 @@ const Cards = () => {
 								adipiscing elit.
 							</p>
 							<a href="#" className="btn btn-primary">
-								Go somewhere
+								Find Out More!
 							</a>
 						</div>
 					</div>
 				</div>
 				<div className="col-sm-3">
 					<div className="card">
+						<svg
+							className="bd-placeholder-img card-img-top"
+							width="100%"
+							height="180"
+							xmlns="http://www.w3.org/2000/svg"
+							role="img"
+							aria-label="Placeholder: Image cap"
+							preserveAspectRatio="xMidYMid slice"
+							focusable="false">
+							<title>Placeholder</title>
+							<rect
+								width="100%"
+								height="100%"
+								fill="#868e96"></rect>
+							<text x="35%" y="50%" fill="#dee2e6" dy=".3em">
+								500 X 325
+							</text>
+						</svg>
 						<div className="card-body">
 							<h5 className="card-title">Card Title</h5>
 							<p className="card-text">
@@ -41,13 +95,31 @@ const Cards = () => {
 								adipiscing elit.
 							</p>
 							<a href="#" className="btn btn-primary">
-								Go somewhere
+								Find Out More!
 							</a>
 						</div>
 					</div>
 				</div>
 				<div className="col-sm-3">
 					<div className="card">
+						<svg
+							className="bd-placeholder-img card-img-top"
+							width="100%"
+							height="180"
+							xmlns="http://www.w3.org/2000/svg"
+							role="img"
+							aria-label="Placeholder: Image cap"
+							preserveAspectRatio="xMidYMid slice"
+							focusable="false">
+							<title>Placeholder</title>
+							<rect
+								width="100%"
+								height="100%"
+								fill="#868e96"></rect>
+							<text x="35%" y="50%" fill="#dee2e6" dy=".3em">
+								500 X 325
+							</text>
+						</svg>
 						<div className="card-body">
 							<h5 className="card-title">Card Title</h5>
 							<p className="card-text">
@@ -55,7 +127,7 @@ const Cards = () => {
 								adipiscing elit.
 							</p>
 							<a href="#" className="btn btn-primary">
-								Go somewhere
+								Find Out More!
 							</a>
 						</div>
 					</div>
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,13 +2,25 @@ import React from "react";
 
 const Navbar = () => {
 	return (
-		<nav className="navbar  navbar-dark bg-dark">
-			<div className="container-fluid">
-				<a className="navbar-brand col-md-8" href="#">
-					Navbar
-				</a>
-			</div>
-		</nav>
+		<div>
+			<nav className="navbar navbar-dark bg-dark">
+				<div className="container-fluid">
+					<div className="container">
+						<a className="navbar-brand">Start Bootstrap</a>
+						<form className="d-flex">
+							<button className="btn btn btn-dark">Home</button>
+							<button className="btn btn btn-dark">About</button>
+							<button className="btn btn btn-dark">
+								Service
+							</button>
+							<button className="btn btn btn-dark">
+								Contact
+							</button>
+						</form>
+					</div>
+				</div>
+			</nav>
+		</div>
 	);
 };
 
